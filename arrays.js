9c3cd94e116553d6af8063c0885f1ef0e6bdfb92
Roberto Busó-García <robertobuso@gmail.com>
2018-06-28 16:41:52 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -32,3 +32,6 @@ function destructivelyRemoveElementFromBeginningOfArray (array) {
   array.shift ()
   return array
 }
+function removeElementFromBeginningOfArray (array) {
+  return array.slice (1)
+}
