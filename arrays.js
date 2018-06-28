40676d1274f092caec095a38565d271fac8d1df3
Roberto Busó-García <robertobuso@gmail.com>
2018-06-28 15:20:49 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,4 +1,5 @@
 var chocolateBars = [ "snickers", "hundred grand", "kitkat", "skittles"];
+
 function addElementToBeginningOfArray (array, element) {
   var myFirstArray = [array]
   mySecondArray = ["foo", ...myFirstArray]
