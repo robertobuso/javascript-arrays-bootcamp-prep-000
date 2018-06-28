12bcf9ae1831a5e13a84ba1c689070a96a9c0837
Roberto Busó-García <robertobuso@gmail.com>
2018-06-28 16:01:40 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -7,8 +7,8 @@ function addElementToBeginningOfArray (array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray (array, element) {
-  var myFirstArray = ['foo', ...myFirstArray]
-  return myFirstArray
+  var myFirstArray = array
+  return ['foo', ...myFirstArray]
 }
 
 function addElementToEndOfArray (array, element) {
